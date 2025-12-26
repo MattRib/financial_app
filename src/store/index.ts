4f70d5d78d0fb3e1101adab1cc8d612a75924996
Matheus Ferro <matheus.rib12@hotmail.com>
2025-12-26 11:11:18 -0300

    feat: add goals store exports and types to the main store index

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,5 +3,7 @@ export { useBudgetsStore } from './budgetsStore'
 export { useCategoriesStore } from './categoriesStore'
 export { useTransactionsStore } from './transactionsStore'
 export { useInvestmentsStore } from './investmentsStore'
+export { useGoalsStore } from './goalsStore'
 
 export type { InvestmentsState, InvestmentSummary, FilterInvestmentDto } from './investmentsStore'
+export type { GoalsState, GoalSummary, FilterGoalDto } from './goalsStore'
